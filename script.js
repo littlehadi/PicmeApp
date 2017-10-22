d1ff0a6f46954fb6c51df7e702dc00e5b044868b
Nicolas Gnyra <nicolas@gnyra.com>
2017-10-22 18:49:09 -0400

    Fix bug

--- script.js
+++ script.js
@@ -3,12 +3,17 @@
     new SmoothScroll('a[href*="#"]');
 
     // make navbar transparent if at top of page
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
+    $(window).on('scroll', onScroll);
 
-        if (scroll > 100)
-            $('#site-navigation').removeClass('nav-top');
-        else
-            $('#site-navigation').addClass('nav-top');
-    });
+    // trigger event so if the page is loaded somewhere else than the top the navbar updates
+    onScroll();
 })();
+
+function onScroll() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll > 100)
+        $('#site-navigation').removeClass('nav-top');
+    else
+        $('#site-navigation').addClass('nav-top');
+}
