c9235fe1934f83c259191855ee565b5788a58b41
mshah87 <mauliksc87@outlook.com>
2017-10-22 19:52:58 -0400

    navbar

---
+++ script.js
@@ -0,0 +1,19 @@
+(function() {
+    // init smooth scroll
+    new SmoothScroll('a[href*="#"]');
+
+    // make navbar transparent if at top of page
+    $(window).on('scroll', onScroll);
+
+    // trigger event so if the page is loaded somewhere else than the top the navbar updates
+    onScroll();
+})();
+
+function onScroll() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll > 100)
+        $('#site-navigation').removeClass('nav-top');
+    else
+        $('#site-navigation').addClass('nav-top');
+}
