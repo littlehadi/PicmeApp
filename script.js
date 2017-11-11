b9c90e87070c8507d486001b416f34988155b788
Nicolas Gnyra <nicolas@gnyra.com>
2017-11-11 16:11:17 -0500

    Implement better scroll reveal
    because all the premade ones don't have the right features

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-(function() {
-    new WOW().init();
 
+
+(function() {
     // init smooth scroll
     new SmoothScroll('a[href*="#"]');
 
@@ -13,9 +13,43 @@
 
 function onScroll() {
     var scroll = $(window).scrollTop();
+    var documentHeight = $(window).height();
 
     if (scroll > 100)
         $('#site-navigation').removeClass('nav-top');
     else
         $('#site-navigation').addClass('nav-top');
+
+    var $elements = [];
+
+    // get all scrollreveal elements that weren't animated yet
+    $('.scrollreveal:not(.revealed)').each(function () {
+        var $self = $(this);
+        var top = $self.offset().top;
+
+        // only reveal if bottom of page is higher than top offset of element
+        if (scroll + documentHeight > top)
+            $elements.push($self);
+    });
+
+    // iterate through elements that should be revealed
+    for (var i = 0; i < $elements.length; i++) {
+        var $self = $elements[i];
+        var top = $self.offset().top;
+        var height = $self.height();
+        var animationName = $self.data('animation-name');
+        var delay = 100; // TODO: should not be hardcoded
+
+        // show element
+        $self.css('visibility', 'visible');
+
+        // only animate if whole element isn't in viewport (indicating page was refreshed at a certain position)
+        if (scroll + documentHeight < top + height) {
+            $self.css('animation-name', animationName);
+            $self.css('animation-delay', i * delay + 'ms'); // add delay between elements being revealed at the same time
+            $self.addClass('animated');
+        }
+
+        $self.addClass('revealed');
+    }
 }
